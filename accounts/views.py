92f02021cc373d92f39da0211377a362ccd1cc51
SHAKEEB KHAN <shakeebsun@gmail.com>
2022-12-23 18:13:06 +0500

    register api done

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from accounts.serializers import UserRegisterSerializer
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 # Create your views here.
@@ -12,7 +13,12 @@ class RegisterAPIView(APIView):
     def post(self, request, format=None):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            serialized_data = serializer.data
-            return Response(serialized_data, status=status.HTTP_201_CREATED)
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            response_data = {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            }
+            # serialized_data = serializer.data
+            return Response(response_data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
